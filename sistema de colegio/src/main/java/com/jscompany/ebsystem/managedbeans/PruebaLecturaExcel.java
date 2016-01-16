1f8f614c04baabdcdf1627116606fd6bc4ba4a56
jsanga <joao_d_20@hotmail.com>
2016-01-16 09:51:04 -0500

    Lectura de archivos excel, aún le falta

---
+++ sistema de colegio/src/main/java/com/jscompany/ebsystem/managedbeans/PruebaLecturaExcel.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jscompany.ebsystem.managedbeans;
+
+import java.io.IOException;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+/**
+ *
+ * @author Joao Sanga
+ */
+@ManagedBean
+@ViewScoped
+public class PruebaLecturaExcel implements Serializable{
+    
+    public static final Long serialVerisonUID = 1L;
+    
+    private UploadedFile file;
+ 
+    @PostConstruct
+    public void init(){
+    
+    }
+    
+    public void handleFileUpload(FileUploadEvent event) {
+        try{
+            if(event != null) {
+                file = event.getFile();
+                FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    
+}
